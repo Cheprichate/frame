03fcc1990df284a6bc52cc70189e4b28a2b9fa5d
Cheprichate <petrovich15071998@mail.ru>
2023-10-26 13:43:30 +0400

    Добавьте файлы проекта.

---
+++ frame.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+int width, height, row, col;
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Ширина и Высота рамки: ";  std::cin >> width >> height;
+
+    for (row = 1; row <= height; row++) {
+        for (col = 1; col <= width; col++) {
+            if (col == 1 or col == width)
+                std::cout << " | ";
+            else if (row == 1 or row == height)
+                std::cout << " - ";
+            else
+                std::cout << "   ";
+        }
+        std::cout << "\n";
+    }
+}
